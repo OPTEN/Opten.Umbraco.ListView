662579c3965802e73d047aa7d9175006089bdb75
Severin d'Heureuse <severin.dheureuse@opten.ch>
2017-05-26 16:06:33 +0200

    PublishedContentExtensions for querying

---
+++ src/Opten.Umbraco.ListView/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+
+namespace Opten.Umbraco.ListView.Extensions
+{
+	public static class HtmlHelperExtensions
+	{
+
+		public static void RenderPublishedContentPartial(this HtmlHelper htmlHelper, IPublishedContent content)
+		{
+			htmlHelper.RenderPartial(GetTemplate(content).VirtualPath, content);
+
+		}
+		public static MvcHtmlString PublishedContentPartial(this HtmlHelper htmlHelper, IPublishedContent content)
+		{
+			return htmlHelper.Partial(GetTemplate(content).VirtualPath, content);
+		}
+
+		private static ITemplate GetTemplate(IPublishedContent content)
+		{
+			return ApplicationContext.Current.Services.FileService.GetTemplate(content.TemplateId);
+		}
+	}
+}
